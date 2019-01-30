2637767b0c044a061f96578be7264bb7a0a7faf9
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-30 14:38:53 +0100

    Added custom event for starting new training.

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-training',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-
+  @Output() trainingStart = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartTraining() {
+    this.trainingStart.emit();
   }
 
 }
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training.component.sass']
 })
 export class TrainingComponent implements OnInit {
-
+  ongoingTraining = false;
   constructor() { }
 
   ngOnInit() {
