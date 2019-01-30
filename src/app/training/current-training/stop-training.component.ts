b9addaecff53e365adc414c2e5039b5d8fa127dd
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-30 15:40:44 +0100

    Passing data to the dialog.

--- src/app/training/current-training/stop-training.component.ts
+++ src/app/training/current-training/stop-training.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-stop-training',
   template: `<h1 mat-dialog-title>Are you sure?</h1>
-  <mad-dialog-actions>
+  <mat-dialog-content>
+    <p>You already got {{passedData.progress}}%</p>
+  </mat-dialog-content>
+  <mat-dialog-actions>
     <button mat-button [mat-dialog-close]="true">Yes</button>
     <button mat-button [mat-dialog-close]="false">No</button>
-  </mad-dialog-actions>`
+  </mat-dialog-actions>`
 })
 export class StopTrainingComponent {
 
+  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) {
+
+  }
 }
