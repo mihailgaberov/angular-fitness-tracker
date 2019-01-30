b096b4dd538878662480fdfc5baabc947b1ba892
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-30 14:23:06 +0100

    Finished new training component.

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
-  styleUrls: ['./new-training.component.sass']
+  styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
 
