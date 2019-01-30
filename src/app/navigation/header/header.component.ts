a1518b9c0bd5724c6d7c3dcd2a75053466c85a58
mihailgaberov <mihail.gaberov@gmail.com>
2019-01-30 10:52:58 +0100

    Splitting the Navigation into separated components.

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  @Output() sidenavToggle = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  onToggleSidenav() {
+    this.sidenavToggle.emit();
+  }
 }
