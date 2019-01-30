7774e06d7c106ab7467e3d25fb96aef0c90c3c12
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-30 16:18:58 +0100

    Started data manipulating features. Added data models. Implementing authentication.

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { User } from './user.model';
+import { AuthData } from './auth-data.model';
+
+export class AuthService {
+  private user: User;
+
+  registerUser(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 10000).toString()
+    };
+  }
+
+  login(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 10000).toString()
+    };
+  }
+
+  logout() {
+    this.user = null;
+  }
+
+  getUser() {
+    return { ...this.user };
+  }
+}
