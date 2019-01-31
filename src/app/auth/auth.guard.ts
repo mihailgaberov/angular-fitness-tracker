c0082076fdd13efe9a92a2a0a4022697b6ee1841
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-31 13:49:22 +0100

    Added route protection - /trianing route.

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,19 @@
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+}
