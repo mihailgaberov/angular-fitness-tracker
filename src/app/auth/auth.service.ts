3cc56b18539fb8357616024750dfb583ccea82da
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-01-31 11:46:55 +0100

    Added Subscription/Subject to track login/logout.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
 export class AuthService {
+  authChange = new Subject<boolean>();
   private user: User;
 
   registerUser(authData: AuthData) {
@@ -9,6 +11,7 @@ export class AuthService {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
+    this.authChange.next(true);
   }
 
   login(authData: AuthData) {
@@ -16,10 +19,12 @@ export class AuthService {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
+    this.authChange.next(true);
   }
 
   logout() {
     this.user = null;
+    this.authChange.next(false);
   }
 
   getUser() {
--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,18 +1,31 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, OnDestroy } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   @Output() sidenavToggle = new EventEmitter<void>();
-  constructor() { }
+  isAuth = false;
+  authSubscription: Subscription;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
   }
 
   onToggleSidenav() {
     this.sidenavToggle.emit();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 }
