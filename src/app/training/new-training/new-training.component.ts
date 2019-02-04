5b8d591875349ea30811cac313da1f4d1e6f959d
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-04 12:16:17 +0100

    Integrated with Firebase and AngularFire.

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TrainingService } from '../training.service';
-import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-new-training',
@@ -12,14 +12,12 @@ import { AngularFirestore } from '@angular/fire/firestore';
 
 
 export class NewTrainingComponent implements OnInit {
-  exercises: Exercise[] = [];
+  exercises: Observable<any>;
 
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.db.collection('availableExercises').valueChanges().subscribe(result => {
-      console.log('result: ', result);
-    });
+    this.exercises = this.db.collection('availableExercises').valueChanges();
   }
 
   onStartTraining(form: NgForm) {
