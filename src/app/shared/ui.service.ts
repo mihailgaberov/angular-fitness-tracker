0a74ef36ca4cb9ac485b9c3d5b75f6e6002bd63e
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-07 14:21:04 +0100

    Refactored using of MatSnackbar component.

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,5 +1,16 @@
 import { Subject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 
+@Injectable()
 export class UiService {
   loadingStateChanged = new Subject<boolean>();
+
+  constructor(private snackbar: MatSnackBar) {}
+
+  showSnackbar(message, action, duration) {
+    this.snackbar.open(message, action, {
+      duration: duration
+    });
+  }
 }
