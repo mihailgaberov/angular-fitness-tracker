23840f3edc8d92c9ba79dbf73fb9fda6f6df7787
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-07 16:00:45 +0100

    Created auth-routing module.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,8 +3,9 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule, MatDatepickerModule } from '@angular/material';
-import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { SharedModule } from '../shared/shared.module';
     MatDatepickerModule,
     MatCheckboxModule,
     AngularFireAuthModule,
-    SharedModule
+    SharedModule,
+    AuthRoutingModule,
+    AngularFireAuth
   ],
   exports: []
 })
