c8797ff059589ec6b12951314a61c4cd3dc3e8bf
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-07 17:01:34 +0100

    Created training-routing module. Lazy load training components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
-import { TrainingComponent } from './training/training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule, MatDatepickerModule, MatListModule, MatNativeDateModule, MatToolbarModule } from '@angular/material';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
-import { StopTrainingComponent } from './training/current-training/stop-training.component';
 import { AuthService } from './auth/auth.service';
 import { TrainingService } from './training/training.service';
 import { AngularFireModule } from '@angular/fire';
@@ -27,14 +22,9 @@ import { AuthModule } from './auth/auth.module';
 @NgModule({
   declarations: [
     AppComponent,
-    TrainingComponent,
-    CurrentTrainingComponent,
-    NewTrainingComponent,
-    PastTrainingsComponent,
     WelcomeComponent,
     HeaderComponent,
-    SidenavListComponent,
-    StopTrainingComponent
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
@@ -54,8 +44,7 @@ import { AuthModule } from './auth/auth.module';
     AuthModule
   ],
   providers: [AuthService, TrainingService, UiService],
-  bootstrap: [AppComponent],
-  entryComponents: [StopTrainingComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule, MatDatepickerModule } from '@angular/material';
-import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -18,8 +18,7 @@ import { AuthRoutingModule } from './auth-routing.module';
     MatCheckboxModule,
     AngularFireAuthModule,
     SharedModule,
-    AuthRoutingModule,
-    AngularFireAuth
+    AuthRoutingModule
   ],
   exports: []
 })
