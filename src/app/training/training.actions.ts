a8be9df4817bdf43f097fac333ae8a7927840db8
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-11 09:44:53 +0100

    Adding Training reducer and actions with payloads.

---
+++ src/app/training/training.actions.ts
@@ -0,0 +1,34 @@
+import { Action } from '@ngrx/store';
+import { Exercise } from './exercise.model';
+
+export const SET_AVAILABLE_TRAININGS = '[Trainings] Set Available';
+export const SET_FINISHED_TRAININGS = '[Trainings] Set Finished';
+export const START_TRAINING = '[Trainings] Set Start training';
+export const STOP_TRAINING = '[Trainings] Stop training';
+
+export class SetAvailableTrainings implements Action {
+  readonly type = SET_AVAILABLE_TRAININGS;
+
+  constructor(public payload: Exercise[]) {
+  }
+}
+
+export class SetFinishedTrainings implements Action {
+  readonly type = SET_FINISHED_TRAININGS;
+
+  constructor(public payload: Exercise[]) {
+  }
+}
+
+export class StartTraining implements Action {
+  readonly type = START_TRAINING;
+
+  constructor(public payload: Exercise) {
+  }
+}
+
+export class StopTraining implements Action {
+  readonly type = STOP_TRAINING;
+}
+
+export type TrainingActions = SetAvailableTrainings | SetFinishedTrainings | StartTraining | StopTraining;
---
+++ src/app/training/training.reducer.ts
@@ -0,0 +1,56 @@
+import { TrainingActions,
+  SET_AVAILABLE_TRAININGS,
+  SET_FINISHED_TRAININGS,
+  START_TRAINING,
+  STOP_TRAINING
+} from './training.actions';
+import { Exercise } from './exercise.model';
+import * as fromRoot from '../app.reducer';
+
+export interface TrainingState {
+  availableExercises: Exercise[];
+  finishedExercises: Exercise[];
+  activeTraining: Exercise;
+}
+
+export interface State extends fromRoot.State {
+  training: TrainingState;
+}
+
+const initialState: TrainingState = {
+  availableExercises: [],
+  finishedExercises: [],
+  activeTraining: null
+};
+
+export function trainingReducer(state = initialState, action: TrainingActions ) {
+  switch (action.type) {
+    case SET_AVAILABLE_TRAININGS:
+      return {
+        ...state,
+        availableExercises: action.payload
+      };
+    case SET_FINISHED_TRAININGS:
+      return {
+        ...state,
+        finishedExercises: action.payload
+      };
+    case START_TRAINING:
+      return {
+        ...this.state,
+        activeTraining: action.payload
+      };
+    case STOP_TRAINING:
+      return {
+        ...state,
+        activeTraining: null
+      };
+    default:
+      return state;
+  }
+
+}
+
+export const getAvailableExercises = (state: TrainingState) => state.availableExercises;
+export const getFinishedExercises = (state: TrainingState) => state.finishedExercises;
+export const getActiveTraining = (state: TrainingState) => state.activeTraining;
